6ea84962b8ab30ce8c604b19ae838a5125915210
Nick Ward <n.ward13@yahoo.com>
2019-01-09 18:22:12 -0700

    add interface with Chief and Proxy

--- contracts/src/Vault.sol
+++ contracts/src/Vault.sol
@@ -1,5 +1,84 @@
-pragma solidity 0.4.24;
+pragma solidity 0.5.2;
 
-contract Vault {
-    
+import "./Proxy.sol";
+import "../lib/DSMath.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+
+// modeled after https://github.com/dydxprotocol/protocol/blob/master/contracts/margin/Vault.sol
+contract Vault is Ownable, DSMath {
+
+    // address public PROXY;
+    Proxy public proxy;
+    address public chief;
+
+    modifier onlyChief() {require(msg.sender == chief, "ccm-vault-auth");_;}
+
+    // TODO: make sure we're emitting sufficient events for users to find these
+    // for pull(). Note that this is the net payouts from all accounts user/contract 
+    // is involved in and is separate from the balance within accounts
+    // user => token address => balance of that token that is held in vault ready
+    mapping(address => mapping(address => uint)) public pulls;
+
+    // Keep track of all our $$$
+    // Note that this is only update when tokens come in or out. No change
+    // when we move balances from accounts to pulls
+    mapping (address => uint) public money;
+
+    // Transfer tokens from user to us
+    function take(address _gem, address src, uint amt) external onlyChief returns (bool) {
+        require(proxy.deal(_gem, src, address(this), amt), "ccm-vault-take-deal-failed");
+
+        money[_gem] = add(money[_gem], amt);
+
+        rich(_gem);
+
+        return true;
+    }
+
+    // Transfer tokens from us to user
+    function give(address _gem, address dst, uint amt) external onlyChief returns (bool) {
+        // This is also asserted by line below, but leaving extra check in here for now
+        require(pulls[dst][_gem] >= amt, "ccm-vault-give-insufficient-balance");
+
+        pulls[dst][_gem] = sub(pulls[dst][_gem], amt);
+        
+        money[_gem] = sub(money[_gem], amt);
+
+        IERC20 gem = IERC20(_gem);
+        // NOTE: this is pull only - i.e. this will only be called from Chief.pull()
+        require(gem.transfer(dst, amt), "ccm-vault-give-transfer-failed");
+
+        // Make sure we're still rich $$
+        rich(_gem);
+
+        return true;
+    }
+
+    // TODO: change return value if not being used
+    // Add to user's pull balance
+    function gift(address _gem, address lad, uint amt) external onlyChief returns (uint) {
+        pulls[lad][_gem] = add(pulls[lad][_gem], amt);
+        return pulls[lad][_gem];
+    }
+
+    // Verify that nothing has gone crazy
+    function rich(address _gem) private view {
+        // If this is false, we have big problems
+        assert(IERC20(_gem).balanceOf(address(this)) >= money[_gem]);
+    }
+
+    ///////////
+    // Owner Functions
+    ///////////
+
+    // Vault has to be deployed before Chief and Proxy, so no constructor
+    // Use this to set other contract addresses
+    function init(address _chief, address _proxy) external onlyOwner {
+        // Prevent owner from changing chief address
+        require(chief == address(0), "ccm-vault-init-no-new-chief");
+        chief = _chief; 
+        proxy = Proxy(_proxy);
+    }
+
 }

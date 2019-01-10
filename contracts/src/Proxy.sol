7bbd6ea6fa8c236aadb4fdcd4f7804c4f7595042
Nick Ward <n.ward13@yahoo.com>
2019-01-09 18:21:24 -0700

    add deal() to transfer to vault

--- contracts/src/Proxy.sol
+++ contracts/src/Proxy.sol
@@ -1,5 +1,39 @@
-pragma solidity 0.4.24;
+pragma solidity 0.5.2;
 
-contract Proxy {
+import "../lib/DSMath.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+
+
+// modeled after https://github.com/dydxprotocol/protocol/blob/master/contracts/margin/TokenProxy.sol
+contract Proxy is DSMath {
     
+    address vault;
+
+    modifier onlyVault() {require(msg.sender == vault, "ccm-proxy-auth");_;}
+
+    constructor(address _vault) public {
+        vault = _vault;
+    }
+
+    // Transfer tokens between addresses. 'dumb' implementation that can
+    // only be called by the Vault contract
+    // deal, pass
+    function deal(address _gem, address src, address dst, uint amt)
+        external 
+        onlyVault 
+        returns (bool)
+    {
+        return IERC20(_gem).transferFrom(src, dst, amt);
+    }
+
+    // TODO: Remove
+    // // Check how much we can transfer from another address to our address
+    // function ours(address _gem, address src) external view returns (uint) {
+    //     IERC20 gem = IERC20(_gem);
+    //     // uint allowance = gem.allowance(src, address(this))
+    //     // uint theirBalance = gem.balanceOf(src);
+        
+    //     // minimum of user's allowance to us and their token balance
+    //     return min(gem.allowance(src, address(this), gem.balanceOf(src));
+    // }
 }

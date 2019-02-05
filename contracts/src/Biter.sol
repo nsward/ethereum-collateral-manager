fa087220a71a7fe2dcc230e0b8690ae980e79b23
Nick Ward <n.ward13@yahoo.com>
2019-02-04 19:44:13 -0700

    add liquidator contract

---
+++ contracts/src/Biter.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.5.3;
+
+import "../lib/AuthTools.sol";
+
+contract BrokerLike {
+    function safe(bytes32) public view returns (bool);
+}
+
+contract VatLike {
+
+}
+
+contract Biter is Ownable {
+    
+    VatLike public vat;
+    BrokerLike public broker;
+
+    constructor(address _vat, address _broker) public {
+        vat = VatLike(_vat);
+        broker = BrokerLike(_broker);
+    }
+
+    function bite(bytes32 acctKey) external {
+        require(!broker.safe(acctKey), "ccm-biter-bite-account-is-safe");
+
+        // TODO: check if at end of call phase?
+
+        // give all heldGem to biter in exchange for biteFee discounted amount of owedGem
+        
+
+
+        // _grab(acctKey);
+    }
+
+    function _grab(bytes32 acctKey) private {
+        // get 
+    }
+}

f5ae9afa740bccea1f4532339e1f73e4e420fa34
Nick Ward <n.ward13@yahoo.com>
2019-02-07 15:37:06 -0700

    add move and call functions

--- contracts/src/Exec.sol
+++ contracts/src/Exec.sol
@@ -1,14 +1,13 @@
 pragma solidity ^0.5.3;
 pragma experimental ABIEncoderV2;
 
-// import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./interfaces/VaultLike.sol";
+import "./events/ExecEvents.sol";
 import "../lib/MathTools.sol";
 import "../lib/AuthTools.sol";
 
 contract VatLike {
-    enum State{ Par, Call, Bit, Old }
-    struct Asset {
+    struct AssetClass {
         uint tax;       // interest rate paid on quantity of collateral not held in dueToken
         uint biteLimit; // Minimum Collateralization Ratio as a ray
         uint biteFee;   // liquidation penalty as a ray, temporarily doubles as discount for collateral
@@ -17,27 +16,31 @@ contract VatLike {
     struct Account {
         uint    lastAccrual;    // time of last interest accrual
         uint    callTab;        // tab due at end of call
+        uint    callEnd;        // end of current callTime. 0 if not called
         uint    callTime;       // time allowed for a call
         uint    owedTab;        // max payout amt
         uint    owedBal;        // balance of owedGem currently held, denominated in owedGem
         uint    heldBal;        // balance of heldGem currently held, denominated in heldGem
-        bytes32 paramsKey;      // hash(admin, user) or hash(admin). Used to get owedGem and asset params
         address heldGem;        // trading token currently held
         address admin;          // admin of the account
         address user;           // user of the account
-        State   state;
+        bytes32 paramsKey;      // hash(admin, user) or hash(admin). Used to get owedGem and asset params
     }
     function owedGems(bytes32) public view returns (address);
     function set(bytes32, bytes32, address) external;
     function set(bytes32, bytes32, bytes32, address, uint) external;
     function set(bytes32, bytes32, uint) external;
-    function doOpen(bytes32, Account memory) public returns (address);    // TODO: calldata??
+    function get(bytes32, bytes32, bytes32) external returns (bytes32);
+    function doOpen(bytes32, Account memory) public returns (bool);    // TODO: calldata??
+    function doCall(bytes32, uint) external returns (uint);
     function safeSetOwedGem(bytes32, address) external;
-    function safeSetAsset(bytes32, address, Asset memory) public;                  // tODO calldata structs?
+    function safeSetAsset(bytes32, address, AssetClass memory) public;                  // tODO calldata structs?
     function owedGemByAccount(bytes32) external view returns (address, bytes32);
+    function addTo(bytes32, bytes32, uint) external;
+    function subFrom(bytes32, bytes32, bytes32, uint) external;
 }
 
-contract Exec is Ownable {
+contract Exec is Ownable, ExecEvents {
 
     uint constant RAY = 10 ** 27;
     mapping (bytes32 => uint) public validTokenPairs;   // keccak256(dueToken, tradeToken) => use
@@ -48,6 +51,23 @@ contract Exec is Ownable {
     constructor(address _vat, address _vault) public {
         vat = VatLike(_vat);
         vault = VaultLike(_vault);
+    }
+
+    function move(address user, address recipient, uint amt) external returns (bool) {
+        bytes32 acctKey = MathTools.k256(msg.sender, user);
+
+        // if enough owedGem in acct to cover amt, do transfer. else, call account
+        if (uint(vat.get("account", "owedBal", acctKey)) < amt) {
+            _call(acctKey, amt);
+        }
+
+        vat.subFrom("account", "owedBal", acctKey, amt);
+        (address owedGem,) = vat.owedGemByAccount(acctKey);
+        
+        // require vault.give(owedGem, recipient, amt);
+        bytes32 claimKey = MathTools.k256(recipient, owedGem);
+        vat.addTo("claim", claimKey, amt);
+        return true;
     }
 
     function open(
@@ -62,7 +82,6 @@ contract Exec is Ownable {
         return _open(owedTab, callTime, user, owedGem, useAdminParams);
     }
 
-    // Assume use Exec Params
     function openWithAdminParams(
         uint owedTab,
         uint callTime,
@@ -73,14 +92,11 @@ contract Exec is Ownable {
         return _open(owedTab, callTime, user, address(0), true);
     }
 
-
-    // called by the managing contract
-    // if _mom == true, _due should be 0
     function _open(
-        uint256 owedTab,         // collateral amt, denominated in dueToken
+        uint256 owedTab,        // collateral amt, denominated in dueToken
         uint256 callTime,       // time allowed after a call
-        address user,           // address of the payer TODO: can't be msg.sender?
-        address owedGemGiven,       // address of the token to pay out in
+        address user,           // address of the payer
+        address owedGemGiven,   // address of the token to pay out in
         bool useAdminParams
     ) 
         private returns (bool) 
@@ -114,12 +130,11 @@ contract Exec is Ownable {
         acct.paramsKey = paramsKey;
         acct.lastAccrual = now;
 
-        // TODO: reentrancy here if bad ERC20? balance added before transfer successful
-
         require(vault.take(owedGem, user, owedTab), "ccm-exec-open-take-failed");
         
-        assert(vat.doOpen(acctKey, acct) == owedGem);   // TODO: remove this assert?
+        require(vat.doOpen(acctKey, acct), "ccm-exec-open-doOpen-failed");
 
+        emit Open(msg.sender, user, owedGem, owedTab);
         return true;       
     }
 
@@ -132,24 +147,34 @@ contract Exec is Ownable {
         return true;
     }
 
-    function addAsset(uint tax, uint biteLimit, uint biteFee, address gem, address user) external returns (bool) {
-        // liquidation penalty must be at least 1
-        require(biteFee >= RAY, "ccm-exec-ngem-axe-invalid");
+    function addAdminAsset(uint tax, uint biteLimit, uint biteFee, address gem) external returns (bool) {
+        return _addAsset(tax, biteLimit, biteFee, gem, address(0));
+    }
+
+    function addAccountAsset(uint tax, uint biteLimit, uint biteFee, address gem, address user) external returns (bool) {
+        require(user != address(0), "ccm-exec-addAccount-asset-invalid-user");
+        return _addAsset(tax, biteLimit, biteFee, gem, user);
+    }
+
+    function _addAsset(uint tax, uint biteLimit, uint biteFee, address gem, address user) internal returns (bool) {
+        // liquidation penalty must be at least 1 (no penalty)
+        require(biteFee >= RAY, "ccm-exec-addAsset-biteFee-invalid");
         // extra collateral has to be able to at least cover penalty
-        require(biteLimit > biteFee, "ccm-exec-ngem-mat-invalid");
+        require(biteLimit >= biteFee, "ccm-exec-addAsset-biteLimit-invalid");
         // RAY is equivalent to no tax
-        require(tax > RAY, "ccm-exec-addAsset-tax-invalid");
+        require(tax >= RAY, "ccm-exec-addAsset-tax-invalid");
 
-        bytes32 acctKey = MathTools.k256(msg.sender, user);
+        bytes32 paramsKey = user == address(0)  ?
+            MathTools.k256(msg.sender)          :
+            MathTools.k256(msg.sender, user);
 
-        (address owedGem, bytes32 paramsKey) = vat.owedGemByAccount(acctKey);
-        require(paramsKey != bytes32(0), "ccm-exec-addAsset-invalid-acct-params");
+        address owedGem = vat.owedGems(paramsKey);
         
         // must be approved token pair
         bytes32 pairKey = MathTools.k256(owedGem, gem);
         require(validTokenPairs[pairKey] == 1, "ccm-exec-addAsset-invalid-token-pair");
 
-        VatLike.Asset memory asset;
+        VatLike.AssetClass memory asset;
         asset.use = 1;
         asset.tax = tax;
         asset.biteFee = biteFee;
@@ -160,9 +185,19 @@ contract Exec is Ownable {
         return true;
     }
 
-    function toggleAsset(bytes32 acctKey, address gem, bool use) external returns (bool) {
+    function toggleAsset(address user, address gem, bool use) external returns (bool) {
+        bytes32 acctKey = MathTools.k256(msg.sender, user);
         vat.set("asset", "use", acctKey, gem, use ? 1 : 0);
         return true;
+    }
+
+    function call(address user, uint callTab) external returns (uint) {
+        bytes32 acctKey = MathTools.k256(msg.sender, user);
+        return _call(acctKey, callTab);
+    }
+
+    function _call(bytes32 acctKey, uint callTab) internal returns (uint) {
+        return vat.doCall(acctKey, callTab);
     }
 
 
@@ -175,74 +210,4 @@ contract Exec is Ownable {
         if (what == "validTokenPair") validTokenPairs[key] = data;
     }
 
-
-    // address owedGem;
-    //     if (useAdminParams) {
-    //         owedGem = vat.doOpenWithAdminOwedGem(accountKey, paramsKey, account);
-    //     } else {
-    //         owedGem = owedGemGiven;
-    //         vat.doOpenWithNewOwedGem(acctKey, paramsKey, owedGem, acct);
-    //     }
-    // // called by the managing contract
-    // // if _mom == true, _due should be 0
-    // function _openWithAllCallsToTheVatContracct(
-    //     uint256 owedTab,         // collateral amt, denominated in dueToken
-    //     uint256 callTime,       // time allowed after a call
-    //     address user,           // address of the payer TODO: can't be msg.sender?
-    //     address owedGemGiven,       // address of the token to pay out in
-    //     bool useAdminParams
-    // ) 
-    //     private returns (bool) 
-    // {
-    //     // Account user can't be zero
-    //     require(user != address(0), "ccm-chief-open-lad-invalid");
-
-    //     // Get owed gem and check that it's not 0? -- does this matter?
-    //     // bc vault.take will fail from address 0 anyway
-    //     address owedGem;
-    //     bytes32 paramsKey;
-    //     if (useAdminParams) {
-    //         paramsKey = MathTools.k256(msg.sender);
-    //         owedGem = vat.owedGems(paramsKey);
-    //     } else {
-    //         paramsKey = MathTools.k256(msg.sender, user);
-    //         owedGem = owedGemGiven;
-    //         vat.set("owedGem", paramsKey, owedGem);
-    //     }
-    //     require(owedGem != address(0), "ccm-exec-open-no-owedGem");
-
-
-    //     // Check that account doesn't exist already. TODO: check who too?
-    //     bytes32 accountKey = MathTools.k256(msg.sender, user);
-    //     require(
-    //         uint(vat.get("account", "lastAccrual", acctKey)) == 0,
-    //         "ccm-exec-open-account-exists"
-    //     );
-
-    //     VatLike.Account memory account;
-    //     account.admin = msg.sender;
-    //     account.owedTab = owedTab;
-    //     account.owedBal = owedTab;
-    //     account.callTime = callTime;
-    //     account.paramsKey = paramsKey;
-    //     account.lastAccrual = now;
-
-    //     // TODO: check this in the vault? Also, don;t need to check. asserted below via SafeMath
-    //     // Check that exec contract is allowed to take funds from user
-    //     uint allowance = vat.allowances(accountKey, owedGem);
-    //     // require(allowance >= owedTab, "ccm-exec-open-insufficient-allowance");
-
-    //     // TODO: do this in vault?
-    //     vat.set("allowance", accountKey, allowance.sub(owedTab));
-
-    //     // Vat calls
-    //     // reads: (maybe)owedGem, lastAccrual / acct empty, allowance
-    //     // writes: (maybe)owedGem, allowance, Account
-    
-
-    //     require(vault.take(owedGem, user, owedTab), "ccm-exec-open-take-failed");
-        
-
-    //     return true;       
-    // }
 }
